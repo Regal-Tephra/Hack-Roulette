5c9bf804bdbd6bc6559a808208905eb27a921da8
Andy Tran <adtran117@gmail.com>
2016-05-26 14:59:53 -0700

    Added basic server route/config and db schema

---
+++ request-handler.js
@@ -0,0 +1,19 @@
+var request = require('request');
+var crypto = require('crypto');
+var bcrypt = require('bcrypt-nodejs');
+// var util = require('../lib/utility');
+
+var db = require('../db/config');
+var User = require('../app/models/user');
+
+exports.loginForm = function(req, res) {
+	res.render('login');
+};
+
+exports.loginUser = function(req, res) {
+
+};
+
+exports.logoutUser = function(req, res) {
+
+};
