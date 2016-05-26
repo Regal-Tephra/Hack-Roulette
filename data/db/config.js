8f013a96828735dc4631704193dcda7c812fb899
Andy Tran <adtran117@gmail.com>
2016-05-26 16:17:04 -0700

    DB connection now works

--- data/db/config.js
+++ data/db/config.js
@@ -2,10 +2,10 @@ var mongoose = require('mongoose');
 var uri = 'mongodb://localhost/green';
 mongoose.connect(uri);
 
-// var db = mongoose.connection;
-// db.on('error', console.error.bind(console, 'connection error:'));
-// db.once('open', function() {
-//   console.log('mongodb connection open')
-// });
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('mongodb connection open')
+});
 
-// module.exports = db;
+module.exports = db;
