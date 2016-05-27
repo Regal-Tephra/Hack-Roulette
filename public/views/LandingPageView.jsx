716699076059b20f8cba93f0fcd4e5e4c11205c0
Andy Tran <adtran117@gmail.com>
2016-05-27 10:55:37 -0700

    Added login button

--- public/views/LandingPageView.jsx
+++ public/views/LandingPageView.jsx
@@ -1,9 +1,23 @@
 class LandingPageView extends React.Component {
     render() {
     	return (
-    	<div><h1>Landing Page View</h1></div>
+    	<div>
+    		<h1>Landing Page View</h1>
+    		<Description />
+    	</div>
         )
     }
 }
 
+var Description = () => (
+	<div className='description'>
+		<Login />
+	</div>
+);
+
+var Login = () => (
+	<div className='login'>
+		<button className='loginbtn'> Login </button>
+	</div>
+);
 window.LandingPageView = LandingPageView;
