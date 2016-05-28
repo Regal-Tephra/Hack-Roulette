38cbb1dc008f7a992f1d1fab73bf5e093ff444b2
Hack Reactor Students <accounts@hackreactor.com>
2016-05-27 21:29:53 -0700

    Made progress on screensharing

--- public/views/ScreenShareView.jsx
+++ public/views/ScreenShareView.jsx
@@ -4,30 +4,57 @@ const socket = io();
 class ScreenShareView extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {value: ''};
+    this.state = {value: ['l']};
     socket.on('text change', function(text){
-      this.setState({value: text});
+      console.log('RECEIVED', text.split('\n'));
+      this.setState({value: text.split('\n')});
     }.bind(this));
     socket.emit('connectUser', this.props.userId);
   }
+  convertToText(html = '') {
+    let text;
+    console.log('html', html);
+    text = html.replace('\n', '');
+    text = text.replace(/<\/p>/gi, '</p>\n');
+    text = text.slice(0, text.length-1)
 
-  handleChange(event) {
+    // Have browser strip html tags for us in false DIV element
+    let tempDiv = document.createElement('div');
+    tempDiv.innerHTML = text;
+    text = tempDiv.textContent || tempDiv.innerText || '';
+    text = _.unescape(text);
+    console.log('text', text);
+    return text;
+  }
+  // convertToDivText(text = '') {
+  //   console.log(text);
+  //   return text.replace(/\n/g, '<br />');
+  // }
+
+  editorUpdated(event) {
+    const cleanedUpText = this.convertToText(event.target.innerHTML);
+    // console.log(cleanedUpText);
     this.setState({
-      value: event.target.value
+      value: [] //cleanedUpText.split('\n')
     });
-    this.setState({temp: event.target.value});
-    socket.emit('change', event.target.value);
+    socket.emit('change', cleanedUpText);
   }
 	render() {
+    console.log(this.state.value.map(line => '<p>'+line+'</p>'));
 		return (
-      <div><input className="form-control"
-      type="text"
-      value={this.state.value}
-      onChange={this.handleChange.bind(this)}
-      /><p>{this.props.userId}</p>
-      <p>{this.state.temp}</p></div>
+      <div>
+        <p>{this.props.userId}</p>
+        <div contentEditable onChange={this.editorUpdated.bind(this)} onBlur={this.editorUpdated.bind(this)}>
+          {this.state.value.map(line => '<p>'+line+'</p>')}
+        </div>
+      </div>
     );
 	}
 }
-
+// {this.state.value.map(line => <p>{line}</p>)}
 window.ScreenShareView = ScreenShareView;
+        // <input className="form-control"
+        // type="text"
+        // value={this.state.value}
+        // onChange={this.handleChange.bind(this)}
+        // />
