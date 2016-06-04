15527ed27224dac4459bbdb956962606084b5951
Felix <felix2feng@gmail.com>
2016-06-04 13:59:01 -0700

    Fixed icecomm.js dependency location

--- public/dependencies/icecomm.js
+++ public/dependencies/icecomm.js
@@ -1,7 +1,6 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict';
 
-console.log('inside icecomm');
 var socket = require('./socket/socketHandler');
 var s = require('./streams/streamHandler');
 var m = require('./utils/methodHandler');
@@ -12702,7 +12701,9 @@ function isLocalStopped() {
 
 function stopLocalStream() {
   if (localStream) {
-    localStream.stop();
+    localStream.getTracks()[0].stop();
+    localStream.getTracks()[1].stop();
+    // localStream.stop();
   }
 }
 
