1d5d4975d2ab21cb75f505e5bd321b390adfc8be
Aaron Freidus <aaronf101@gmail.com>
2016-06-04 13:49:31 -0700

    When commited adds client side user logic

--- public/views/AppView.jsx
+++ public/views/AppView.jsx
@@ -16,7 +16,11 @@
 
 const Router = ReactRouter.Router;
 const Route = ReactRouter.Route;
+<<<<<<< HEAD
 
+=======
+const userIdOptions = ['Greg', 'Thomas', 'Andy', 'Erika', 'Selena', 'Josh', 'William', 'Brittany'];
+>>>>>>> When commited adds client side user logic
 class AppView extends React.Component {
   constructor(props) {
     super(props);
@@ -27,6 +31,7 @@ class AppView extends React.Component {
     this.state = {
       sessionData: {},
     };
+<<<<<<< HEAD
   }
 
   handleMainSubmit(data) {
@@ -41,15 +46,53 @@ class AppView extends React.Component {
         <ScreenShareView sessionData={this.state.sessionData} />
       );
     };
+=======
+    // ajax request to see if user is logged in
+    // if not displayName/profileUrl/username will be undefiend
+    $.ajax({
+      url: '/loginCheck',
+      type: 'GET',
+      success: (data) => {
+        const parsedData = JSON.parse(data);
+        this.setState(
+          {
+            displayName: parsedData.displayName,
+            profileUrl: parsedData.profileUrl,
+            username: parsedData.username,
+          }
+        );
+      },
+    });
+    // Currently Hardcoded
+    this.userId = userIdOptions[Math.floor(Math.random() * userIdOptions.length)];
+    // this.handleChange = this.handleChange.bind(this);
+    // this.pages = {
+    //   Landingpage: <LandingPageView />,
+    //   Mainpage: <MainpageView userId={this.userId} />,
+    //   Session: <ScreenShareView userId={this.userId} />,
+    //   Feedback: <FeedbackView />,
+    //   Helper: <HelperView />,
+    // };
+  }
+  // {this.pages[this.state.currentPage]}
+  // TODO: Build in navbar toggle
+  render() {
+    const context = this;
+    const requireAuth = (nextState, replaceState) => {
+      if (!context.state.username) {
+        replaceState({ nextPathname: nextState.location.pathname }, '/login');
+      }
+    };
+    // pass user data in as a prop on 
     return (
       <div className="container">
         <div className="title">Hack Roulette</div>
         <Router>
-          <Route path="/" component={MainpageView} onMainSubmit={this.handleMainSubmit} />
+          <Route path="/" component={MainpageView} onMainSubmit={this.handleMainSubmit} onEnter={requireAuth} />
           <Route path="/login" component={LandingPageView} />
-          <Route path="/screenshare" component={ScreenShareViewWrap} sessionData={this.state.sessionData} />
-          <Route path="/feedback" component={FeedbackView} />
-          <Route path="/helper" component={HelperView} />
+          <Route path="/screenshare" component={ScreenShareViewWrap} sessionData={this.state.sessionData} onEnter={requireAuth} />
+          <Route path="/feedback" component={FeedbackView} onEnter={requireAuth} />
+          <Route path="/helper" component={HelperView} onEnter={requireAuth} />
         </Router>
         <footer className="footer container-fluid text-center">
           <p className="col-lg-8">{"Made with <3"}</p>
