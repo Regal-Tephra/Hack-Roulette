e68dab9eab0cd7c2e8bf682a6bc25adc76ef8341
Aaron Freidus <aaronf101@gmail.com>
2016-06-06 11:55:17 -0700

    improved nav-bar styling

--- public/views/pageComponents/NavbarView.jsx
+++ public/views/pageComponents/NavbarView.jsx
@@ -6,39 +6,38 @@
 const Link = ReactRouter.Link;
 
 window.NavbarView = (props) => (
-  <nav className="navbar navbar-default">
+  <nav className="navbar navbar-default light-themed-background">
+    <img src="../img/hack-roulette-logo-1.png" className="logo" />
+    <div className="navbar-buttons">
+      <button className="btn btn-default navbar-btn pull-right" onClick={props.videoHandler}>
+        <Link to="/login">
+          Log Out
+        </Link>
+      </button>
 
-    <p className="navbar-text">Hack Roulette</p>
+      <button className="btn btn-default navbar-btn pull-right" onClick={props.videoHandler}>
+        <Link to="/helper">
+          Become a Helper
+        </Link>
+      </button>
 
-    <button className="btn btn-default navbar-btn pull-right" onClick={props.videoHandler}>
-      <Link to="/login">
-        Log Out
-      </Link>
-    </button>
+      <button className="btn btn-default navbar-btn pull-right" onClick={props.videoHandler}>
+        <Link to="/">
+          [TEMP] HOME
+        </Link>
+      </button>
 
-    <button className="btn btn-default navbar-btn pull-right" onClick={props.videoHandler}>
-      <Link to="/helper">
-        Become a Helper
-      </Link>
-    </button>
+      <button className="btn btn-default navbar-btn pull-right" onClick={props.videoHandler}>
+        <Link to="/screenshare">
+          [TEMP] Screenshare
+        </Link>
+      </button>
 
-    <button className="btn btn-default navbar-btn pull-right" onClick={props.videoHandler}>
-      <Link to="/">
-        [TEMP] HOME
-      </Link>
-    </button>
-
-    <button className="btn btn-default navbar-btn pull-right" onClick={props.videoHandler}>
-      <Link to="/screenshare">
-        [TEMP] Screenshare
-      </Link>
-    </button>
-
-    <button className="btn btn-default navbar-btn pull-right" onClick={props.videoHandler}>
-      <Link to="/feedback">
-        [TEMP] Feedback
-      </Link>
-    </button>
-
+      <button className="btn btn-default navbar-btn pull-right" onClick={props.videoHandler}>
+        <Link to="/feedback">
+          [TEMP] Feedback
+        </Link>
+      </button>
+    </div>
   </nav>
 );
